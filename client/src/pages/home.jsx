74a7540c8803c992cb585bc1e38f68ef6bcb191f
mars <imidcen@gmail.com>
2026-01-28 17:39:53 +0000

    testing for tailwindcss

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,7 +1,11 @@
 const Home = () => {
   return (
     <div>
-
+      <div>
+        <div className="bg-black">
+          <p className="text-white">hello</p>
+        </div>
+      </div>
     </div>
   );
 };
