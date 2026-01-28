f4797efa9ee807236d41721a5f9f09244ba66686
mars <imidcen@gmail.com>
2026-01-28 18:14:51 +0000

    hopefully this new setup works for vercel now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
-import { HashRouter as Router } from 'react-router-dom'
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/home'; 
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-
       </Routes>
     </Router>
   );
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,13 +1,13 @@
 const Home = () => {
   return (
     <div>
-      <div>
-        <div className="bg-black">
-          <p className="text-white">hello</p>
-        </div>
+      <div className="bg-green-500 text-white p-8 mt-4">
+        <h1 className="text-3xl font-bold">Tailwind Test</h1>
+        <p className="mt-4">This should be green if Tailwind works</p>
       </div>
     </div>
   );
 };
+
 
 export default Home;
