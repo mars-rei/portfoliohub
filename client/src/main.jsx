405c74178fff3c9792739f181f15f009e6f0ecf5
mars <imidcen@gmail.com>
2026-01-28 17:27:15 +0000

    forming frontend in react using setup from my reactSpotifyThemedPortfolio

---
+++ client/src/main.jsx
@@ -0,0 +1,9 @@
+{/* don't edit this file */}
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <App />
+)
