b928bed8dc07c2c0303c97ead21ace8a8527b6c5
mars <imidcen@gmail.com>
2026-01-29 21:13:51 +0000

    navbar done

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,26 @@
+const NavBar = () => {
+  return (
+    <div className="bg-[#ebfff2] flex justify-between items-center px-4 py-4 border-b-2 border-[##1f1f1f]">
+      <div className="text-[#003c66] font-fustat-extrabold text-3xl">
+        PortfolioHub
+      </div>
+
+      <div className="flex items-center text-[#1f1f1f] space-x-4 font-fustat-bold text-xl">
+        <div>
+          About
+        </div>
+        <div>
+          Documentation
+        </div>
+        <div>
+          <img 
+            src="/userIcon.svg" 
+            className="w-8 h-8"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,13 +1,11 @@
+import NavBar from '../components/navbar.jsx';
+
 const Home = () => {
   return (
     <div>
-      <div className="bg-green-500 text-white p-8 mt-4">
-        <h1 className="text-3xl font-bold">Tailwind Test</h1>
-        <p className="mt-4">This should be green if Tailwind works</p>
-      </div>
+      <NavBar />
     </div>
   );
 };
-
 
 export default Home;
