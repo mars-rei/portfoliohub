334a3898448873f41951133303fa8da0035711ec
mars <imidcen@gmail.com>
2026-02-02 21:43:24 +0000

    finally got setup with inertia working (php laravel setup on laptop successful!)

--- combined/portfolioHub/database/migrations/0001_01_01_000001_create_cache_table.php
+++ combined/portfolioHub/database/migrations/0001_01_01_000001_create_cache_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cache', function (Blueprint $table) {
+        Schema::create('laravel.cache', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->mediumText('value');
             $table->integer('expiration')->index();
         });
 
-        Schema::create('cache_locks', function (Blueprint $table) {
+        Schema::create('laravel.cache_locks', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->string('owner');
             $table->integer('expiration')->index();
@@ -29,7 +29,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cache');
-        Schema::dropIfExists('cache_locks');
+        Schema::dropIfExists('laravel.cache');
+        Schema::dropIfExists('laravel.cache_locks');
     }
 };
--- combined/portfolioHub/resources/js/Pages/Home.jsx
+++ combined/portfolioHub/resources/js/Pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <>
-            <h1 className="title">Hello user</h1>
+            <h1 className="title">Hello user, it's finally working!!!!</h1>
         </>
     )
 }
